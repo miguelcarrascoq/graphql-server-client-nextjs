cd8bee4fd6dde16e4118a0a2ff26f6d3d23af566
Miguel Carrasco Q <miguel.carrasco.quijada@gmail.com>
2024-02-15 00:07:48 -0300

    Add Apollo Client and GraphQL query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Link from 'next/link';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 export default function Home() {
+
+  const client = new ApolloClient({
+    uri: 'https://graphql-server-client-nextjs.netlify.app/api/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  client
+    .query({
+      query: gql`
+        query ExampleQuery {
+          hello
+        }
+      }
+    `,
+    })
+    .then((result) => console.log(result));
+
   return (
-    <Link href="/api/graphql">
-      GraphQL here...
-    </Link>
+    <>
+      <Link href="/api/graphql">
+        GraphQL here...
+      </Link>
+      <p>...and see the console</p>
+    </>
   );
 }
