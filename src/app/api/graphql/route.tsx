b2b86b148a87e6656f8a4c57ba9df51902148caa
Miguel Carrasco Q <miguel.carrasco.quijada@gmail.com>
2024-02-14 22:48:12 -0300

    change Landing page plugin

--- src/app/api/graphql/route.tsx
+++ src/app/api/graphql/route.tsx
@@ -2,6 +2,7 @@ import { NextRequest } from 'next/server';
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { gql } from 'graphql-tag';
+import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
 
 const resolvers = {
     Query: {
@@ -18,6 +19,15 @@ const typeDefs = gql`
 const server = new ApolloServer({
     resolvers,
     typeDefs,
+    plugins: [
+        // Install a landing page plugin based on NODE_ENV
+        process.env.NODE_ENV === 'production'
+            ? ApolloServerPluginLandingPageProductionDefault({
+                graphRef: 'my-graph-id@my-graph-variant',
+                footer: false,
+            })
+            : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    ],
 });
 
 const handler = startServerAndCreateNextHandler<NextRequest>(server, {
